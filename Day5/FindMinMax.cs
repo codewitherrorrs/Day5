35b2057559e4604c6c16f640fb471a81f8ed49d8
“codewitherrors” <codewitherrors1@gmail.com>
2023-05-10 10:37:27 +0530

    Add : Find Maximum Value Using Double by Using Generics

--- Day5/FindMinMax.cs
+++ Day5/FindMinMax.cs
@@ -7,7 +7,23 @@ using System.Threading.Tasks;
 namespace Day5
 {
     public class FindMinMax<T> where T : IComparable<T>
-    {       
+    {
+
+        public void MaxFind(int firstValue, int secondValue,int thirdValue)
+        {
+            if(firstValue > secondValue && firstValue > thirdValue)
+            {
+                Console.WriteLine($"First Value is greater : {firstValue}");
+            }
+            else if (secondValue > firstValue && secondValue > thirdValue)
+            {
+                Console.WriteLine($"Second Value is greater : {secondValue}");
+            }
+            else
+            {
+                Console.WriteLine($"Third value is greater : {thirdValue}");
+            }
+        }
         public void FindMax(T firstValue,T secondValue,T thirdValue)
         {
             if(secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
--- Day5/Program.cs
+++ Day5/Program.cs
@@ -5,12 +5,18 @@
         public static void Main(string[] args)
         {
             FindMinMax<int> findMinMax = new FindMinMax<int>();
+            FindMinMax<double> findMax = new FindMinMax<double>();
             //Max With Second Number.
             findMinMax.FindMax(20, 300, 50);
+            findMax.FindMax(20.08, 300.00, 50.365);
             //Max With First Number.
             findMinMax.FindMax(2000, 300, 50);
+            findMax.FindMax(20.08, 30.00, 500.365);
             //Max With Third Number.
             findMinMax.FindMax(2000, 300, 5000);
+            findMax.FindMax(200.08, 3.00, 50.365);
+            //Using Normal Method.
+            findMinMax.MaxFind(20,30,50);
 
         }
     }
