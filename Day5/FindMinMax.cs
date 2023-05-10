c4cb02d9e079ae905678cf726a849b75c9b11a61
“codewitherrors” <codewitherrors1@gmail.com>
2023-05-10 10:22:53 +0530

    Add : Find Max Value Using Integer Parameter Using Generics With Test Case

---
+++ Day5/FindMinMax.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5
+{
+    public class FindMinMax<T> where T : IComparable<T>
+    {       
+        public void FindMax(T firstValue,T secondValue,T thirdValue)
+        {
+            if(secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+            { 
+                Console.WriteLine($"Second is gretaer : {secondValue}");
+            }
+            else if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
+            {
+                Console.WriteLine($"Third is gretaer : {thirdValue}");
+            }
+            else
+            {
+                Console.WriteLine(firstValue);
+            }
+        }
+    }
+}
---
+++ Day5/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Day5
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            FindMinMax<int> findMinMax = new FindMinMax<int>();
+            //Max With Second Number.
+            findMinMax.FindMax(20, 300, 50);
+            //Max With First Number.
+            findMinMax.FindMax(2000, 300, 50);
+            //Max With Third Number.
+            findMinMax.FindMax(2000, 300, 5000);
+
+        }
+    }
+}
